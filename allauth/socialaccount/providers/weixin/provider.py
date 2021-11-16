4e36235c0a3f23a2f4671aec9b6f2e349ef5f894
朱穆穆 <mumu.zhu@feat.com>
2021-11-16 16:20:41 +0800

    feat(weixin) Add unionid support and multi app support

--- allauth/socialaccount/providers/weixin/provider.py
+++ allauth/socialaccount/providers/weixin/provider.py
@@ -1,5 +1,11 @@
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+from allauth.socialaccount import app_settings
+from allauth.utils import get_request_param
+from django.db.models import JSONField
+from django.db.models.functions import Cast
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.exceptions import PermissionDenied
 
 
 class WeixinAccount(ProviderAccount):
@@ -25,6 +31,99 @@ class WeixinProvider(OAuth2Provider):
 
     def extract_common_fields(self, data):
         return dict(username=data.get("nickname"), name=data.get("nickname"))
+
+    def get_app(self, request):
+        # NOTE: Avoid loading models at top due to registry boot...
+        from allauth.socialaccount.models import SocialApp
+
+        apps = self.get_apps()
+
+        state = getattr(request, 'state', {})
+        app_id = state.get('app_id') or request.GET.get('app_id')
+
+        if app_id:
+            for app in apps:
+                if app.client_id == app_id:
+                    return app
+            else:
+                raise SocialApp.DoesNotExist
+        elif len(apps) > 1:
+            raise SocialApp.MultipleObjectsReturned
+        else:
+            return apps[0]
+
+    def get_apps(self):
+        # NOTE: Avoid loading models at top due to registry boot...
+        from allauth.socialaccount.models import SocialApp
+
+        settings = app_settings.PROVIDERS.get(self.id, {})
+        configs = settings.get('APPS', [])
+        config = settings.get('APP')
+        if config:
+            configs.append('config')
+
+        apps = []
+        if configs:
+            for config in configs:
+                app = SocialApp(provider=self.id)
+                for field in ["client_id", "secret", "key", "certificate_key"]:
+                    setattr(app, field, config.get(field))
+                apps.append(app)
+        else:
+            site = get_current_site(self.request)
+            apps = SocialApp.objects.filter(
+                    provider=self.id, sites__id=site.id
+                    )
+        return apps
+
+    @classmethod
+    def get_social_login(kls):
+        # NOTE: Avoid loading models at top due to registry boot...
+        from allauth.socialaccount.models import SocialAccount
+        SocialLogin = super().get_social_login()
+
+        class WeixinSocialLogin(SocialLogin):
+            def lookup(self):
+                """ lookup unionid if openid not found
+                """
+                super().lookup()
+
+                if self.is_existing:
+                    return
+
+                unionid = self.account.extra_data.get('unionid')
+                if not unionid:
+                    return
+
+                try:
+                    a = SocialAccount.objects.alias(
+                            extra_json=Cast('extra_data', JSONField())
+                            ).get(
+                        provider=self.account.provider,
+                        extra_json__unionid=unionid
+                    )
+
+                    # Update account
+                    self.account.user = a.user
+                    self.user = self.account.user
+                    self.account.save()
+                    # Update token
+                    if app_settings.STORE_TOKENS and self.token and self.token.app.pk:
+                        assert not self.token.pk
+                        self.token.account = self.account
+                        self.token.save()
+                except SocialAccount.DoesNotExist:
+                    pass
+
+            @classmethod
+            def state_from_request(cls, request):
+                state = super().state_from_request(request)
+                app_id = get_request_param(request, "app_id", None)
+                if app_id:
+                    state['app_id'] = app_id
+                return state
+
+        return WeixinSocialLogin
 
 
 provider_classes = [WeixinProvider]
--- allauth/socialaccount/providers/weixin/tests.py
+++ allauth/socialaccount/providers/weixin/tests.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from urllib.parse import parse_qs, urlparse
 
+from allauth.account.signals import user_logged_in
 from allauth.socialaccount.providers import registry
-from allauth.socialaccount.tests import create_oauth2_tests
-from allauth.tests import MockedResponse
+from allauth.socialaccount.tests import create_oauth2_tests, setup_app
+from allauth.tests import MockedResponse, mocked_response
+from django.urls import reverse
 
 from .provider import WeixinProvider
 
@@ -31,3 +34,84 @@ class WeixinTests(create_oauth2_tests(registry.by_id(WeixinProvider.id))):
  "sex": 1,
  "unionid": "ohHrhwKnD9TOunEW0eKTS45vS5Qo"}""",
         )  # noqa
+
+
+class AnotherWeixinTests(WeixinTests):
+    def make_openid(self, app_id=None):
+        if not app_id:
+            return "ohS-VwAJ9GEXlplngwybJ3Z-ZHrI"
+
+        return app_id +  "ohS-VwAJ9GEXlplngwybJ3Z-ZHrI"
+
+    def get_mocked_response(self, app_id=None, unionid="ohHrhwKnD9TOunEW0eKTS45vS5Qo"):
+        return MockedResponse(
+            200,
+            """
+{"access_token":
+ "OezXcEiiBSKSxW0eoylIeO5cPxb4Ks1RpbXGMv9uiV35032zNHGzXcld-EKsSScE3gRZMrUU78skCbp1ShtZnR0dQB8Wr_LUf7FA-H97Lnd2HgQah_GnkQex-vPFsGEwPPcNAV6q1Vz3uRNgL0MUFg",
+ "city": "Pudong New District",
+ "country": "CN",
+ "expires_in": 7200,
+ "headimgurl":
+ "http://wx.qlogo.cn/mmopen/VkvLVEpoJiaibYsVyW8GzxHibzlnqSM7iaX09r6TWUJXCNQHibHz37krvN65HR1ibEpgH5K5sukcIzA3r1C4KQ9qyyX9XIUdY9lNOk/0",
+ "language": "zh_CN",
+ "nickname": "某某某",
+ "openid": "%s",
+ "privilege": [],
+ "province": "Shanghai",
+ "refresh_token":
+ "OezXcEiiBSKSxW0eoylIeO5cPxb4Ks1RpbXGMv9uiV35032zNHGzXcld-EKsSScEbMnnMqVExcSpj7KRAuBA8BU2j2e_FK5dgBe-ro32k7OuHtznwqqBn5QR7LZGo2-P8G7gG0eitjyZ751sFlnTAw",
+ "scope": "snsapi_login",
+ "sex": 1,
+ "unionid": "%s"}""" % (self.make_openid(app_id), unionid),
+        )  # noqa
+
+    def login(self, resp_mock, process="login", with_refresh_token=True, app_id=None):
+        qs = dict(process=process)
+        if app_id:
+            qs['app_id'] = app_id
+        resp = self.client.get(
+            reverse(self.provider.id + "_login"), qs
+        )
+        p = urlparse(resp["location"])
+        q = parse_qs(p.query)
+        complete_url = reverse(self.provider.id + "_callback")
+        self.assertGreater(q["redirect_uri"][0].find(complete_url), 0)
+        response_json = self.get_login_response_json(
+            with_refresh_token=with_refresh_token
+        )
+        if isinstance(resp_mock, list):
+            resp_mocks = resp_mock
+        else:
+            resp_mocks = [resp_mock]
+
+        with mocked_response(
+            MockedResponse(200, response_json, {"content-type": "application/json"}),
+            *resp_mocks,
+        ):
+            resp = self.client.get(complete_url, self.get_complete_parameters(q))
+        return resp
+
+    def test_multi_app(self):
+        self.another_app = setup_app(self.provider)
+        self.another_app.client_id = 'another_app'
+        self.another_app.save()
+
+        login_users = []
+
+        def on_login_user(sender, request, user, **kwargs):
+            sociallogin = kwargs["sociallogin"]
+            self.assertEqual(sociallogin.account.provider, WeixinProvider.id)
+            self.assertEqual(sociallogin.account.user, user)
+            login_users.append(user)
+
+        user_logged_in.connect(on_login_user)
+
+        self.login(self.get_mocked_response(app_id=self.app.client_id), app_id=self.app.client_id)
+
+        self.assertEqual(len(login_users), 1)
+
+        self.login(self.get_mocked_response(app_id=self.another_app.client_id), app_id=self.another_app.client_id)
+
+        self.assertEqual(len(login_users), 2)
+        self.assertEqual(*login_users)
