c66d262cd22b2e78787772fc6332b72d4ef2a0ba
朱穆穆 <mumu.zhu@feat.com>
2025-05-16 09:39:47 +0800

    chore(weixin): super().to_str() already tries to lookup nickname in extra_data

--- allauth/socialaccount/providers/weixin/provider.py
+++ allauth/socialaccount/providers/weixin/provider.py
@@ -23,9 +23,6 @@ class WeixinAccount(ProviderAccount):
     def get_avatar_url(self):
         return self.account.extra_data.get("headimgurl")
 
-    def to_str(self):
-        return self.account.extra_data.get("nickname", super().to_str())
-
     def get_unionid(self):
         return self.account.extra_data.get("unionid", None)
 
